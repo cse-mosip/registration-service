ec8db0a45cb467b84f8d87311bb15abf6cc9bcb9
Shanaaz Ahamed <77115237+imshaaz21@users.noreply.github.com>
2023-07-14 13:54:11 +0530

    initial

---
+++ middleware/authorization.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+
+export const isAuth = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (authorization) {
+    // Bearer xxx => xxx
+    const token = authorization.slice(7, authorization.length);
+    jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
+      if (err) {
+        res.status(401).send({ message: 'Invalid Token' });
+      } else {
+        // Do the next thing
+        next();
+      }
+    });
+  } else {
+    res.send(401).send({ message: 'No Token' });
+  }
+};
---
+++ routes/api/index.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+
+//router.use('/registration', registrationRoutes);
+
+module.exports = router;
