42f4c2ce64644aabaaaa1c5d1f80b02e4ce67bde
shangopi <gopinath.19@cse.mrt.ac.lk>
2023-07-15 13:35:44 +0530

    controller, router initalized for authorization

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const sequelize = require('./config/connection.js');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+  
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
