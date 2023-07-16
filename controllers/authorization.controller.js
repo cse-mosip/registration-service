f247203fe4f9be823e6933bab3b79b18d03d1cbd
Shanaaz Ahamed <77115237+imshaaz21@users.noreply.github.com>
2023-07-16 12:14:46 +0530

    delete end point added

--- controllers/authorization.controller.js
+++ controllers/authorization.controller.js
@@ -10,30 +10,47 @@ exports.signIn = (req, res) => {
     }
 
     const { email, password } = req.body;
-    
+
     if (!(email && password)) {
         res.status(400).send("Email or Password is Missing");
     } else {
-           // Validate if user exist in our database
-            // const user = await User.findOne({ email });
-        
+        // Validate if user exist in our database
+        // const user = await User.findOne({ email });
+
         const salt = bcrypt.genSaltSync(saltRounds);
-        encryptedPassword =  bcrypt.hashSync(process.env.PASSWORD, salt);
+        encryptedPassword = bcrypt.hashSync(process.env.PASSWORD, salt);
         try {
             if (!bcrypt.compareSync(password, encryptedPassword)) {
-                res.status(400).json("Password does not match !");                
+                res.status(400).json("Password does not match !");
             } else {
                 const token = jwt.sign(
-                    {email_id : email },
+                    { email_id: email },
                     process.env.JWT_SECRET,
                     {
-                      expiresIn: "2h",
+                        expiresIn: "2h",
                     }
-                  );
-                  res.status(201).json(token);
+                );
+                res.status(201).json(token);
             }
-        }  catch (err) {
+        } catch (err) {
             console.log(err);
-        }             
+        }
     }
 }
+
+exports.deleteUser = (req, res) => {
+    const userId = req.body.userId;
+    if (!userId) {
+        return res.status(400).send("User Id is Missing");
+    }
+    // Validate if user exist in our database
+    User.findByIdAndRemove(userId, (err, user) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+        } else if (!user) {
+            res.status(404).json({ message: "User not found" });
+        } else {
+            res.status(200).json({ message: "User deleted successfully" });
+        }
+    });
+};
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const apiStudentRoutes = require("./student.routes");
 const authorizationRoutes = require("./authorization.routes");
+const authorizationController = require("../../controllers/authorization.controller");
+const { isAuth } = require("./../../middleware/authorization");
 
 router.use("/student", apiStudentRoutes);
 router.use("/authorization", authorizationRoutes);
