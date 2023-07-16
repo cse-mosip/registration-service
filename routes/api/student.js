aeada91a194e9ab85d8854fb4dfb4dbb801396b6
sarraj-sareef <sareefsarraj1999@gmail.com>
2023-07-16 10:45:09 +0530

    API for specific user

--- routes/api/student.js
+++ routes/api/student.js
@@ -32,4 +32,68 @@ router.post("/", [isAuth], (req, res) => {
     res.status(400).json({ data: {}, msg: "Invalid form data" });
   }
 });
+const { Sequelize, DataTypes } = require('sequelize');
+
+
+const STUDENTS = sequelize.define('STUDENTS', {
+  
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  firstname: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastname: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  faculty: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  tableName: 'STUDENTS', 
+  timestamps: false, 
+});
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection to the database has been established successfully.');
+
+    await STUDENTS.sync();
+
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
+router.get("/:id",(req, res) => {
+  sequelize.sync().then(() => {
+
+    STUDENTS.findOne({
+        where: {
+            id : "1"
+        }
+    }).then(results => {
+        console.log(results);
+        res.status(200).json(results);
+    }).catch((error) => {
+        console.error('Failed to retrieve data : ', error);
+        res.status(500).json({ error: 'Failed to retrieve data' });
+    });
+
+}).catch((error) => {
+    console.error('Unable to create table : ', error);
+});
+
+
+});
 module.exports = router;
