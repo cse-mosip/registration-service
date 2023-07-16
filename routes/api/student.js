c928d15a3c0d4431c7e5ab596521258c0ae51ee7
sarraj-sareef <sareefsarraj1999@gmail.com>
2023-07-16 08:59:04 +0530

    get all user api done

--- routes/api/student.js
+++ routes/api/student.js
@@ -3,6 +3,7 @@ const { isAuth } = require("./../../middleware/authorization");
 const { formValidation } = require("./../../validation/form");
 const RegistationForm = require("./../../dto/RegistationForm");
 const router = express.Router();
+const sequelize = require("./../../config/connection.js");
 
 router.post("/", [isAuth], (req, res) => {
   let { index, email, firstName, lastName, faculty } = req.body;
@@ -32,4 +33,66 @@ router.post("/", [isAuth], (req, res) => {
     res.status(400).json({ data: {}, msg: "Invalid form data" });
   }
 });
+
+const { Sequelize, DataTypes } = require('sequelize');
+
+
+const STUDENTS = sequelize.define('STUDENTS', {
+  
+  ID: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  EMAIL: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  FIRSTNAME: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  LASTNAME: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  FACULTY: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  tableName: 'STUDENTS', 
+  timestamps: false, 
+});
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection to the database has been established successfully.');
+
+    await STUDENTS.sync();
+
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
+
+router.get("/",(req, res) => {
+  sequelize.sync().then(() => {
+    STUDENTS.findAll().then(results => {
+      console.log(results);
+      res.status(200).json(results);
+    }).catch((error) => {
+      console.error('Failed to retrieve data : ', error);
+      res.status(500).json({ error: 'Failed to retrieve data' });
+    });
+
+  }).catch((error) => {
+    console.error('SOMETHING WRONG : ', error);
+  });
+
+});
+
+
 module.exports = router;
