e7b5dc4d90fdc61e87e2d42b7e2542c2a3f4ccb7
Mohamed Ishad <ishad.19@cse.mrt.ac.lk>
2023-07-16 19:55:56 +0530

    Refactor code

--- controllers/student.controller.js
+++ controllers/student.controller.js
@@ -1,28 +1,48 @@
-const { formValidation } = require("./../validation/form");
+const {formValidation} = require("./../validation/form");
 const RegistationForm = require("./../dto/RegistationForm");
 const Student = require("../model/student.model");
+const sequelize = require("../config/connection");
 
 exports.registation = (req, res) => {
-  let { index, email, firstName, lastName, faculty } = req.body;
-  console.log(req.body);
-  let registationForm = new RegistationForm(
-    index,
-    email,
-    firstName,
-    lastName,
-    faculty
-  );
-  let isValid = formValidation(registationForm);
-  console.log(isValid);
-  if (isValid) {
-    Student.create({...registationForm}).then((student) => {
-        res.status(201).json({ data: student, msg: "Add new user" });
-    }).catch((err) => {
-      console.log(err);
-      res.status(409).json({ data: {}, msg: err.message });
-    });
-  } else {
-    //response
-    res.status(400).json({ data: {}, msg: "Invalid form data" });
-  }
+    let {index, email, firstName, lastName, faculty} = req.body;
+    console.log(req.body);
+    let registationForm = new RegistationForm(
+        index,
+        email,
+        firstName,
+        lastName,
+        faculty
+    );
+    let isValid = formValidation(registationForm);
+    console.log(isValid);
+    if (isValid) {
+        sequelize.sync().then(() => {
+            Student.create({...registationForm}).then((student) => {
+                res.status(201).json({data: student, msg: "Add new user"});
+            }).catch((err) => {
+                console.log(err);
+                res.status(409).json({data: {}, msg: err.message});
+            });
+        }).catch((error) => {
+            console.error('SOMETHING WRONG : ', error.message);
+        });
+    } else {
+        //response
+        res.status(400).json({data: {}, msg: "Invalid form data"});
+    }
 };
+
+exports.getAll = (req, res) => {
+    sequelize.sync().then(() => {
+        Student.findAll().then(results => {
+            console.log(results);
+            res.status(200).json(results);
+        }).catch((error) => {
+            console.error('Failed to retrieve data : ', error);
+            res.status(500).json({error: 'Failed to retrieve data'});
+        });
+
+    }).catch((error) => {
+        console.error('SOMETHING WRONG : ', error.message);
+    });
+}
