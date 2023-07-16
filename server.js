31ed103a82c4ee10281b6403477562b9d7b5b1f3
Nethmi Jayakody <nethmi.19@cse.mrt.ac.lk>
2023-07-16 14:39:52 +0530

    [fe-be-integration] registration integration

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const routes = require("./routes");
+const cors = require('cors');
 // import sequelize connection
 const sequelize = require("./config/connection.js");
 
@@ -8,6 +9,7 @@ const PORT = 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use(routes);
 app.listen(3000, () => {
