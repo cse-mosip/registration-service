ff90c06a02d6534d9d797e7e60201ca405d77a55
sarraj-sareef <sareefsarraj1999@gmail.com>
2023-07-16 19:49:15 +0530

    added API for spectific user profile

--- routes/api/student.routes.js
+++ routes/api/student.routes.js
@@ -4,6 +4,71 @@ const router = express.Router();
 const studentController = require("../../controllers/student.controller");
 const sequelize = require("../../config/connection");
 
+
 router.post("/", [isAuth], studentController.registation);
 router.get("/",[isAuth], studentController.getAll);
 module.exports = router;
+const { Sequelize, DataTypes } = require('sequelize');
+
+
+const STUDENTS = sequelize.define('STUDENTS', {
+  
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  firstname: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastname: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  faculty: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  tableName: 'STUDENTS', 
+  timestamps: false, 
+});
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection to the database has been established successfully.');
+
+    await STUDENTS.sync();
+
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
+router.get("/:id",(req, res) => {
+  sequelize.sync().then(() => {
+
+    STUDENTS.findOne({
+        where: {
+            id : "1"
+        }
+    }).then(results => {
+        console.log(results);
+        res.status(200).json(results);
+    }).catch((error) => {
+        console.error('Failed to retrieve data : ', error);
+        res.status(500).json({ error: 'Failed to retrieve data' });
+    });
+
+}).catch((error) => {
+    console.error('Unable to create table : ', error);
+});
+
+
+});
