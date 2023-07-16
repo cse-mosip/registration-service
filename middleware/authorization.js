637ca7fe85adeeb70834cd58678cb190a8e503d2
athapaththuamsrb <rukshanathapaththu07@gmail.com>
2023-07-16 16:59:34 +0530

    test

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,22 +1,22 @@
 const jwt = require("jsonwebtoken");
 
 const isAuth = (req, res, next) => {
-  next();
-  // const authorization = req.headers.authorization;
-  // if (authorization) {
-  //   // Bearer xxx => xxx
-  //   const token = authorization.slice(7, authorization.length);
-  //   jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
-  //     if (err) {
-  //       res.status(401).send({ message: "Invalid Token" });
-  //     } else {
-  //       // Do the next thing
-  //       next();
-  //     }
-  //   });
-  // } else {
-  //   res.status(401).send({ message: "No Token" });
-  // }
+  // next();
+  const authorization = req.headers.authorization;
+  if (authorization) {
+    // Bearer xxx => xxx
+    const token = authorization.slice(7, authorization.length);
+    jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
+      if (err) {
+        res.status(401).send({ message: "Invalid Token" });
+      } else {
+        // Do the next thing
+        next();
+      }
+    });
+  } else {
+    res.status(401).send({ message: "No Token" });
+  }
 };
 module.exports = {
   isAuth,
