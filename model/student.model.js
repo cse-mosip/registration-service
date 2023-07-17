68bd868624654c2c2152fdedf688ce51899c8b93
SupunDhananjaya <supun.19@cse.mrt.ac.lk>
2023-07-17 09:58:28 +0530

    Update student.model.js

--- model/student.model.js
+++ model/student.model.js
@@ -1,45 +1,45 @@
-const {Sequelize, DataTypes} = require("sequelize");
-const sequelize = require("../config/connection");
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
 const Student = sequelize.define(
-    "Student",
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        index: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        firstName: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        lastName: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        faculty: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        department: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
+  'Student',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        tableName: "student",
-        timestamps: false,
-    }
+    index: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    faculty: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    department: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: 'student',
+    timestamps: false,
+  }
 );
 
 module.exports = Student;
