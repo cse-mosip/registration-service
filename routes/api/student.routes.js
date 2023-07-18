26540dd61515b991f25cd6d943e3ac3280c99708
AKSHILMY <akshilmy@gmail.com>
2023-07-18 22:53:35 +0530

    Extended validation for image upload - single file upload only

--- routes/api/student.routes.js
+++ routes/api/student.routes.js
@@ -2,11 +2,12 @@ const express = require("express");
 const {isAuth} = require("../../middleware/authorization");
 const router = express.Router();
 const studentController = require("../../controllers/student.controller");
+const {upload} = require("../../uploads/storage");
 
 router.post("/index", [isAuth], studentController.getInfoByIndexList);
 router.get("/:id", [isAuth], studentController.getById);
 router.delete("/:id", [isAuth], studentController.deleteById);
 router.get("/", [isAuth], studentController.getAll);
-router.post("/", [isAuth], studentController.registation);
+router.post("/", [isAuth,upload.single('image')], studentController.registation);
 router.put("/:id", [isAuth], studentController.editUser);
 module.exports = router;
