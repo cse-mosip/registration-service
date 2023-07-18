323f5d7c1d3a9bc7d7a4d72da566ea17ec406c30
AKSHILMY <akshilmy@gmail.com>
2023-07-18 22:52:55 +0530

    Added 'image' field to registration form

--- dto/StudentRegistationForm.js
+++ dto/StudentRegistationForm.js
@@ -1,10 +1,11 @@
 module.exports = class StudentRegistationForm {
-  constructor(index, email, firstName, lastName, faculty, department) {
+  constructor(index, email, firstName, lastName, faculty, department,image) {
     this.index = index;
     this.email = email;
     this.firstName = firstName;
     this.lastName = lastName;
     this.faculty = faculty;
     this.department = department;
+    this.image = image;
   }
 };
--- model/student.model.js
+++ model/student.model.js
@@ -35,6 +35,10 @@ const Student = sequelize.define(
       type: DataTypes.STRING,
       allowNull: false,
     },
+    image:{
+      type: DataTypes.STRING,
+      allowNull : true,
+    }
   },
   {
     tableName: 'student',
