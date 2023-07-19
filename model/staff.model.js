7cb7ad009a848de1d460bf72dbd7093823326f13
Shanaaz Ahamed <77115237+imshaaz21@users.noreply.github.com>
2023-07-19 19:00:05 +0530

    staff model added

--- model/staff.model.js
+++ model/staff.model.js
@@ -1,10 +1,99 @@
-class Staff {
-    constructor(staff) {
-            this.first_name = staff.first_name,
-            this.last_name = staff.last_name,
-            this.telephone = staff.telephone,
-            this.email = staff.email,
-            this.password = user.password;
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+class Staff extends Model { 
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
     }
 }
-export default Staff;
+
+Staff.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        phoneNumber: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        nic: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        image: {
+            type: DataTypes.STRING, //TODO: change to BLOB
+            allowNull: false,
+        },
+        deptId:{
+            type: DataTypes.STRING,
+            references: {
+                model: 'department',
+                key: 'id',
+                onDelete: 'cascade',
+            },
+        },
+        dob: {
+            type: DataTypes.DATEONLY,
+            allowNull: false,
+        },
+        address: {
+            type: DataTypes.STRING,
+            allowNull: true, //TODO: Change to false
+        },
+        gender: {
+            type: DataTypes.STRING,
+            allowNull: true, //TODO: Change to false
+        },
+        role :{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        fingerprintId: {
+            type: DataTypes.STRING,
+            references: {
+                model: 'fingerprint',
+                key: 'fingerprintId',
+                onDelete: 'cascade',
+            },
+        },
+    },
+    {
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'staff',
+    }
+);
+
+module.exports = Staff;
