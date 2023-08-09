b10f0f18227150ef67e7ff250a6718d026b1c0a6
Mohamed Ishad <ishad.19@cse.mrt.ac.lk>
2023-08-09 14:02:00 +0530

    Fixed build failures

--- controllers/student.controller.js
+++ controllers/student.controller.js
@@ -32,7 +32,7 @@ exports.registation = (req, res) => {
         sequelize
             .sync()
             .then(() => {
-                Student.create({ ...registationForm })
+                Student.create({...registationForm })
                     .then((student) => {
                         res.status(201).json({
                             data: student,
@@ -98,10 +98,10 @@ exports.deleteById = (req, res) => {
         .sync()
         .then(() => {
             Student.destroy({
-                where: {
-                    id: studentId,
-                },
-            })
+                    where: {
+                        id: studentId,
+                    },
+                })
                 .then((results) => {
                     console.log(results);
                     res.status(200).json(results);
@@ -114,4 +114,44 @@ exports.deleteById = (req, res) => {
             console.error("SOMETHING WRONG : ", error.message);
             res.status(500).json({ error: error.message });
         });
+};
+
+exports.editUser = (req, res) => {
+    let id = req.params.id;
+    let { index, email, firstName, lastName, faculty, department } = req.body;
+    console.log(req.body);
+    let registationForm = new StudentRegistationForm(
+        index,
+        email,
+        firstName,
+        lastName,
+        faculty,
+        department
+    );
+    let isValid = studentFormValidation(registationForm);
+    console.log(isValid);
+    if (isValid) {
+        sequelize
+            .sync()
+            .then(() => {
+                Student.update({...registationForm }, {
+                        where: {
+                            id: id,
+                        },
+                    })
+                    .then((student) => {
+                        res.status(201).json({ data: student, msg: "Update the User" });
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                        res.status(409).json({ data: {}, msg: err.message });
+                    });
+            })
+            .catch((error) => {
+                console.error("SOMETHING WRONG : ", error.message);
+            });
+    } else {
+        //response
+        res.status(400).json({ data: {}, msg: "Invalid form data" });
+    }
 };
